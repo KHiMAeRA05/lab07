1f91bfb75203f193c4681bc023f4b21ac86e76b1
rhtyme <rhtyme@gmail.com>
2022-03-04 17:23:45 +0500

    added semicolons

--- tests.cpp
+++ tests.cpp
@@ -12,27 +12,27 @@ TEST_CASE("Ex01", "[exercise]") {
     int y = 0;
     multiples(y, 1, 2);
 
-    REQUIRE(y == 4)
+    REQUIRE(y == 4);
 
     float y2 = 0.0;
     multiples<float>(y2, 1.5, 3.0);
 
-    REQUIRE(y2 == 10)
+    REQUIRE(y2 == 10);
 }
 
 TEST_CASE("Ex02", "[exercise]") {
     Calculator<int> intCalc(2, 1);
 
-    REQUIRE(intCalc.add() == 3)
-    REQUIRE(intCalc.multiply() == 2)
-    REQUIRE(intCalc.divide() == 2)
-    REQUIRE(intCalc.subtract() == 1)
+    REQUIRE(intCalc.add() == 3);
+    REQUIRE(intCalc.multiply() == 2);
+    REQUIRE(intCalc.divide() == 2);
+    REQUIRE(intCalc.subtract() == 1);
 
     Calculator<float> floatCalc(12.0, 3.0);
 
-    REQUIRE(floatCalc.add() == 15.0)
-    REQUIRE(floatCalc.multiply() == 36.0)
-    REQUIRE(floatCalc.divide() == 4.0)
-    REQUIRE(floatCalc.subtract() == 9.0)
+    REQUIRE(floatCalc.add() == 15.0);
+    REQUIRE(floatCalc.multiply() == 36.0);
+    REQUIRE(floatCalc.divide() == 4.0);
+    REQUIRE(floatCalc.subtract() == 9.0);
 
 }
