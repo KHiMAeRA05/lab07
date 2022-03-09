d3ec44d31aa407f3f0ead92c19b4e5998dd098d5
Aleksandra <45710112+KHiMAeRA05@users.noreply.github.com>
2022-03-09 17:37:13 +0300

    Update ex03.hpp

--- ex03.hpp
+++ ex03.hpp
@@ -2,27 +2,24 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    double bill;
-    double tiptotal;
-    double total;
-    double tip;
-
+template <typename T>
+T init() {
+    T bill;
+    T tiptotal;
+    T total;
+    T tip;
     cout<<"\n\nWelcome to tip Calculator\n";
     cout <<"------------------------------\n";
     cout << "Enter bill amount: "<<"$";
-    cin>> bill;
+    cin >> bill;
 
-    cout<< "Enter Tip: "<<"%";
-    cin>> tip;
+    cout << "Enter Tip: "<<"%";
+    cin >> tip;
 
-    if (tip<20)
-    {
+    if (tip < 20) {
         cout<< "\n Oh you can give a better tip than that!\n";
         char ans;
-        do
-        {
+        do {
             cout<< "Do you want to continue (Y/N)?\n";
             cout<< "You must type a 'Y' or an 'N'.\n";
             cin >> ans;
@@ -30,14 +27,18 @@ int main()
         while((ans !='Y')&&(ans !='N')&&(ans !='y')&&(ans !='n'));
         return tip;
     }
-    else
-    {
+    else {
 
         tiptotal = bill * (tip/100.);
         total = (tiptotal + bill);
         cout << "The total Bill with Tip is: "<<"$"<< total << "\n\n";
         cout << "You are helping me through College!! Thankyou!!\n";
     }
+
+}
+
+int main(){
+    init<double>();
 
     return 0;
 }
